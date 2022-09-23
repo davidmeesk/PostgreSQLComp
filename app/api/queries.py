817ffeb482e1d15ae235ac9c82152e43ce74135c
David <kdavidk98@gmail.com>
2022-09-23 15:54:44 -0300

    legendaries query

--- app/api/queries.py
+++ app/api/queries.py
@@ -87,3 +87,29 @@ def before_request():
 def teardown_request(exception=None):
     diff = time.time() - g.start
     print("query time: " + str(diff))
+
+@bp.route('/legendaries', methods=['GET'])
+def legendaries():
+    results = db.engine.execute("""
+        SELECT *
+        FROM species
+        WHERE legendary = true
+    """)
+    response = []
+    for result in results:
+        response.append({
+            "id":result[0],
+            "dex":result[1],
+            "name":result[2],
+            "type1":result[3],
+            "type2":result[4],
+            "HP":result[5],
+            "attack":result[6],
+            "defense":result[7],
+            "sp_atk":result[8],
+            "sp_def":result[9],
+            "speed":result[10],
+            "gen":result[11],
+            "legendary":result[12],
+        })
+    return json.dumps(response)
